0c39f2a1104702f5d53c47027f29fdd978c753fe
Julian Chuan <315405283+cx-julian-chuan@users.noreply.github.com>
2026-09-22 16:53:31 -0400

    Checkmarx Bot - Auto Pull Request

--- src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
@@ -9,9 +9,9 @@ package org.cysecurity.cspf.jvl.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -53,13 +53,25 @@ public class Register extends HttpServlet {
              {
                     if(con!=null && !con.isClosed())
                                {
-                                  
-                                   Statement stmt = con.createStatement();  
-                                  stmt.executeUpdate("INSERT into users(username, password, email, About,avatar,privilege,secretquestion,secret) values ('"+user+"','"+pass+"','"+email+"','"+about+"','default.jpg','user',1,'"+secret+"')");
-                                       stmt.executeUpdate("INSERT into UserMessages(recipient, sender, subject, msg) values ('"+user+"','admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')");
-             
+                                   // Use PreparedStatement to prevent SQL injection by parameterizing all user-supplied values
+                                   PreparedStatement pstmt = con.prepareStatement(
+                                       "INSERT into users(username, password, email, About, avatar, privilege, secretquestion, secret) VALUES (?, ?, ?, ?, 'default.jpg', 'user', 1, ?)");
+                                   pstmt.setString(1, user);
+                                   pstmt.setString(2, pass);
+                                   pstmt.setString(3, email);
+                                   pstmt.setString(4, about);
+                                   pstmt.setString(5, secret);
+                                   pstmt.executeUpdate();
+                                   pstmt.close();
+
+                                   PreparedStatement msgStmt = con.prepareStatement(
+                                       "INSERT into UserMessages(recipient, sender, subject, msg) VALUES (?, 'admin', 'Hi', 'Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')");
+                                   msgStmt.setString(1, user);
+                                   msgStmt.executeUpdate();
+                                   msgStmt.close();
+
                                     response.sendRedirect("index.jsp");
-                                    
+
                                }
                     else
                     {
